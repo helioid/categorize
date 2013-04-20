30a23554e2b5eadcdbda11b9a85b938b07a7299a
pld <peter@helioid.com>
2013-04-20 12:43:09 -0400

    simplify and name by convention

--- ext/categorize/extconf.rb
+++ ext/categorize/extconf.rb
@@ -3,11 +3,5 @@
 # Loads mkmf which is used to make makefiles for Ruby extensions
 require 'mkmf'
 
-# Give it a name
-extension_name = 'bow'
-
-# The destination
-dir_config(extension_name)
-
 # Do the work
-create_makefile(extension_name)
+create_makefile('categorize/categorize')
