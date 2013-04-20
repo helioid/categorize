3463ff6fce71a0c5eac51cd6bcb4fd2bb69a8842
pld <peter@helioid.com>
2013-04-20 14:04:38 -0400

    add option to not include C code

--- lib/categorize.rb
+++ lib/categorize.rb
@@ -1,5 +1,8 @@
 # encoding: utf-8
 
+# The C extension is listed first.
+require 'categorize/categorize' unless ENV['NO_C_INCLUDE']
+
 require 'categorize/models/abstract_model'
 require 'categorize/models/bag_of_words'
 require 'categorize/models/cluster'
