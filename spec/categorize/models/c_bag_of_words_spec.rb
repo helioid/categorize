ca9cbca411500b5fc131508847c228623e577417
pld <peter@helioid.com>
2013-04-28 04:05:46 -0400

    add test for c BOW algorithm

--- spec/categorize/models/c_bag_of_words_spec.rb
+++ spec/categorize/models/c_bag_of_words_spec.rb
@@ -5,6 +5,18 @@ require 'spec_helper'
 module Categorize
   module Models
     describe CBagOfWords do
+      before :each do
+        @model = CBagOfWords.new
+        @documents = DocumentHelper::Documents.map do |s|
+          Categorize::Model.preprocess(s)
+        end
+      end
+
+      it 'should be importable' do
+        results = @model.model(@documents)
+
+        results.length.should eq(3)
+      end
     end
   end
 end
