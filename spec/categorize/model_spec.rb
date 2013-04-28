7d6278c76f0bdf2ba0391cd4f614a70a161d524a
pld <peter@helioid.com>
2013-04-28 03:57:14 -0400

    test for C version

--- spec/categorize/model_spec.rb
+++ spec/categorize/model_spec.rb
@@ -8,12 +8,12 @@ module Categorize
       documents = DocumentHelper::Documents
 
       @doc_multiplier = 5
-      @strings = documents.join(' ').downcase.split(/\W/)
+      @strings = documents.join(' ').downcase
       @documents = documents * @doc_multiplier
     end
 
     it 'categorizes based on number of documents' do
-      query = 'test'
+      query = DocumentHelper::Query
       results = Model.make_model(query, @documents)
 
       results.each do |category, documents|
@@ -22,7 +22,7 @@ module Categorize
       end
     end
 
-    it 'categorizes helioid' do
+    it 'consider max bucket setting' do
       documents = DocumentHelper::Helioid
       bag_of_words = Models::BagOfWords.new
       bag_of_words.max_buckets = 3
@@ -30,6 +30,14 @@ module Categorize
 
       results = Model.make_model('helioid', documents, bag_of_words)
 
+      results.values.flatten.min.should eq(0)
+      results.length.should eq(3)
+    end
+
+    it 'categorizes using C version' do
+      results = Model.make_model_c(@documents)
+
+      results.values.flatten.min.should eq(0)
       results.length.should eq(3)
     end
   end
